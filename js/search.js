c44842dff911bbd8372594bc8dbec9cad4290929
Matilda Olsson <matildaolsson@MacBook-Air-som-tillhor-Matilda.local>
2026-05-08 23:29:50 +0200

    started working on search icon, changed css and added js

---
+++ js/search.js
@@ -0,0 +1,14 @@
+const searchButton = document.getElementById("search-button");
+const searchPanel = document.getElementById("search-panel");
+const exitButton = document.getElementById("search-exit-button");
+
+searchButton.addEventListener("click", () => {
+
+    searchPanel.style.right = "0";
+
+
+});
+
+exitButton.addEventListener("click", () => {
+    searchPanel.style.right = "-400px";
+}); 
